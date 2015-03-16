0fbd7a70b0786621b01bf5596025ce39d7adfb84
Carlos Interone <carlos@mostlyclicks.com>
2015-03-16 10:45:56 -0500

    add link from teaser to blog

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  before_filter :load_athletes, :load_home_athletes
+  before_filter :load_athletes, :load_home_athletes, :load_blog_posts
 
   protected
 
@@ -11,6 +11,10 @@ class ApplicationController < ActionController::Base
 
   def load_home_athletes
     @home_athletes = Refinery::Athletes::Athlete.limit(3)
+  end
+
+  def load_blog_posts
+    @home_blog = Refinery::Blog::Post.limit(3)
   end
 
 end
