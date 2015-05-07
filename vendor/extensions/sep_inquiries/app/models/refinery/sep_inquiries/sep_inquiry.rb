0f5d6e3d04320016609d012baf3f9d76009d1473
Carlos Interone <carlos@mostlyclicks.com>
2015-05-07 10:48:46 -0500

    sep form validations and label changes

--- vendor/extensions/sep_inquiries/app/models/refinery/sep_inquiries/sep_inquiry.rb
+++ vendor/extensions/sep_inquiries/app/models/refinery/sep_inquiries/sep_inquiry.rb
@@ -12,6 +12,19 @@ module Refinery
       # Add some validation here if you want to validate the user's input
       # We have validated the first string field for you.
       validates :first_name, :presence => true
+      validates :last_name, :presence => true
+      validates :age, :numericality => true
+      validates :phone, :presence => true
+      validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
+      validates :gender, inclusion: ["Male", "Female"]
+      validates :city, :presence => true
+      validates :state_province, :presence => true
+      validates :country, :presence => true
+      validates :contact_time_1, :presence => true
+      validates :area_of_focus, :presence => true
+      validates :event_times, :presence => true
+      validates :athletic_background, :presence => true
+      validates :athletic_vision, :presence => true
 
       GENDERS = ["Male", "Female"]
 
